96d6ba205a34c3cde79768f3289bd60e50d5ad86
Allan Crain <allankcrain@gmail.com>
2019-12-11 22:20:06 -0600

    Day 10

--- asteroidfield.rb
+++ asteroidfield.rb
@@ -8,7 +8,7 @@ class AsteroidField
         @asteroids = []
         @field.each_with_index do |row, y|
             row.split('').each_with_index do |char, x|
-                @asteroids << [x,y] if char=='#'
+                @asteroids << [x,y] if char=='#' || char=='X'
             end
         end
     end
@@ -17,31 +17,54 @@ class AsteroidField
         @asteroids
     end
 
+    # Get the list of asteroids relative to the given asteroid.
+    # Does a little bit of trig so we have angles and distances to
+    # the other asteroids relative to the given asteroid as if it were
+    # the polar origin
+    def get_relative_asteroids(ast)
+      # Get the polar coordinates of each asteroid relative to the base asteroid
+      transteroids = @asteroids.select { |tgt| tgt != ast}.map do |tgt|
+          # Translated X and Y
+          tx = tgt[0] - ast[0]
+          ty = tgt[1] - ast[1]
+          # Calculate the angle, then change phi so anything left of -pi/2 (up)
+          # comes after pi.
+          phi = Math.atan2(ty, tx)
+          phi = Math::PI + Math::PI + phi if phi < -Math::PI/2
+          {
+              real: tgt,
+              r: Math.sqrt(tx*tx + ty*ty),
+              phi: phi
+          }
+      end
+      transteroids
+    end
+
     # Return the list of other asteroids that ast can see
     def can_see(ast)
-        # Get the polar coordinates of each asteroid relative to the base asteroid
-        transteroids = @asteroids.select { |tgt| tgt != ast}.map do |tgt| 
-            # Translated X and Y
-            tx = tgt[0] - ast[0]
-            ty = tgt[1] - ast[1]
-            {
-                real: tgt,
-                r: Math.sqrt(tx*tx + ty*ty),
-                phi: Math.atan2(ty, tx)
-            }
-        end
+        transteroids = get_relative_asteroids(ast)
 
         # Now we filter the asteroids list to only have the
         # asteroid with the lowest 'r' value for a given phi.
         # (i.e., the closest asteroid at a given angle)
-        filteroids = transteroids.select do |tgt|
+        transteroids.select do |tgt|
             !transteroids.any? { |tgt2| tgt2[:phi] == tgt[:phi] && tgt2[:r] < tgt[:r] }
         end
-
-        filteroids.map { |trn| trn[:real]}
     end
 
+    # List of potential targets for zapping, ordered rotationally starting from north
+    def targets(ast)
+      can_see(ast).sort{ |a,b| a[:phi] <=> b[:phi] }
+    end
+
+    # Get the asteroid that has the highest visibility of other asteroids
     def highest_visibility
-        @asteroids.map {|ast| can_see(ast).count}.sort.last
+        @asteroids.map {|ast| [ast, can_see(ast).count]}.sort{ |a,b| a[1]<=>b[1] }.last
+    end
+
+    # Blow the given asteroid out of the sky. Removes it from the asteroids list
+    # so it will no longer come back in can_see or relative_asteroids returns.
+    def zap(asteroid)
+      @asteroids.delete(asteroid)
     end
 end
--- day10.rb
+++ day10.rb
@@ -1,7 +1,23 @@
 #!/usr/bin/env ruby
-
 require './asteroidfield'
 
 field = AsteroidField.new(ARGF)
 
-puts "Highest visibility: #{field.highest_visibility}"
+base, targets = field.highest_visibility
+puts "Highest visibility: #{targets} seen by #{base}"
+
+targets = field.targets(base)
+
+puts "Zappin' 'roids"
+counter = 1
+while counter <= 200 && targets.length > 0 do
+    # Aim for the next target
+    target = targets.shift
+    # Zap the victim
+    field.zap(target[:real])
+    # Refresh our target list if necessary
+    targets = field.targets(base) unless targets.length > 0
+    counter += 1
+end
+
+puts "Last target is #{target[:real]}, so our answer is #{target[:real][0]*100 + target[:real][1]}"
